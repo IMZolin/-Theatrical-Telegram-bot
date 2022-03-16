7a2eb35ee045156d791b202bf49983e08df49ceb
IMZolin <zolin5269@gmail.com>
2022-03-16 21:22:13 +0300

    Formated parsing file.Convert json to xlsx. Delete unnecessary files

--- parser.py
+++ parser.py
@@ -2,72 +2,81 @@ import requests
 import json
 import pandas as pd
 from bs4 import BeautifulSoup
-url = 'https://www.afisha.ru/spb/schedule_theatre/'
-headers = {
-    "accept": "*/*",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
-}
-def pars():
-    pd.read_json("all_performance_dict.json", lines=True).to_excel("performance_list.xlsx")
 
-# req = requests.get(url, headers)
-# src =req.text
-# #print(src)
-# with open("index.html", "w", encoding="utf-8") as file:
-#     file.write(src)
+def GetHTML():
+    url = 'https://www.afisha.ru/spb/schedule_theatre/'
+    headers = {
+        "accept": "*/*",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+    }
+    req = requests.get(url, headers)
+    src =req.text
+    #print(src)
+    with open("index.html", "w", encoding="utf-8") as file:
+        file.write(src)
+    with open('index.html', 'r', encoding='utf-8') as file:
+        src = file.read()
+    soup = BeautifulSoup(src, 'lxml')
+    return soup
 
+def ConvertToExcel():
+    with open("all_performance_dict.json", "r", encoding='utf-8') as file:
+        df_json = pd.read_json("all_performance_dict.json")
+        df_json.to_excel("all_performance_dict.xlsx")
 
+def InformationInFile():
+    pages = 49
+    performance_data_list = []
+    with open("index.html", encoding='utf-8') as file:
+        src = file.read()
+    soup = BeautifulSoup(src, "lxml")
+    performances = soup.find_all("div", class_="_1kwbj lkWIA _2Ds3f")
+    for performance in performances:
+        performance_name = performance.find("a", class_="_3NqYW DWsHS _3lmHp wkn_c")
+        performance_theatre = performance.find("a", class_="_3NqYW G_0Rp")
+        performance_genre = performance.find("a", class_="_3NqYW G_0Rp").find_next()
+        performance_min_discription = performance.find("div", class_="_3Di4D _2qUBY")
+        performance_rating = performance.find("span", class_="_1g1fp ql7kl _3EZKc _1JPCS _20dAu _3VWPW _12fWX")
+        if (performance.find("span", class_="_1gC4P")):
+            performance_day_all = performance.find("span", class_="_1gC4P")
+            str_day = performance_day_all.get_text()
+            performance_day = ""
+            i = 0
+            while str_day[i] != ",":
+                performance_day += str_day[i]
+                i += 1
 
-pages = 49
-performance_data_list = []
-with open("index.html", encoding='utf-8') as file:
-    src = file.read()
-soup = BeautifulSoup(src,"lxml")
-performances = soup.find_all("div", class_="_1kwbj lkWIA _2Ds3f")
-for performance in performances:
-    performance_name = performance.find("a", class_="_3NqYW DWsHS _3lmHp wkn_c")
-    performance_theatre = performance.find("a", class_="_3NqYW G_0Rp")
-    performance_genre = performance.find("a", class_="_3NqYW G_0Rp").find_next()
-    performance_min_discription = performance.find("div", class_="_3Di4D _2qUBY")
-    performance_rating = performance.find("span", class_="_1g1fp ql7kl _3EZKc _1JPCS _20dAu _3VWPW _12fWX")
-    if(performance.find("span", class_="_1gC4P")):
-        performance_day_all = performance.find("span", class_="_1gC4P")
-        str_day = performance_day_all.get_text()
-        performance_day = ""
-        i = 0
-        while str_day[i] != ",":
-            performance_day += str_day[i]
-            i += 1
+        if (performance.find("span", class_="_21BWX _2O1ut _1lIKZ bsB4F")):
+            performance_price = performance.find("span", class_="_21BWX _2O1ut _1lIKZ bsB4F").find("span")
+        performance_urls = "https://www.afisha.ru" + performance.find("div", class_="_1V-Pk").find("a").get("href")
 
+        if (
+                performance_name and performance_min_discription and performance_theatre and performance_rating and performance_urls and performance_price):
+            performance_data_list.append(
+                {
+                    "Название спектакля:": performance_name.text,
+                    "День и время:": performance_day,
+                    "Жанр:": performance_genre.text,
+                    "Рейтинг:": performance_rating.text,
+                    "Цена:": performance_price.text,
+                    "Театр:": performance_theatre.text,
+                    "Описание:": performance_min_discription.text,
+                    "URL спектакля:": performance_urls,
+                }
+            )
+    print(performance_data_list)
+    with open("all_performance_dict.json", "w", encoding='utf-8') as file:
+        json.dump(performance_data_list, file, indent=4, ensure_ascii=False)
+        # data=json.load(file)
+        # df=pd.DataFrame(data)
+        # df.to_excel("performance_list.xlsx")
 
-    if(performance.find("span", class_="_21BWX _2O1ut _1lIKZ bsB4F")):
-        performance_price = performance.find("span", class_="_21BWX _2O1ut _1lIKZ bsB4F").find("span")
-    performance_urls = "https://www.afisha.ru" + performance.find("div", class_="_1V-Pk").find("a").get("href")
+def main():
+    GetHTML()
+    InformationInFile()
+    ConvertToExcel()
 
-    if(performance_name and performance_min_discription and performance_theatre and performance_rating and performance_urls and performance_price):
-        performance_data_list.append(
-            {
-                "Название спектакля:": performance_name.text,
-                "День и время:": performance_day,
-                "Жанр:": performance_genre.text,
-                "Рейтинг:": performance_rating.text,
-                "Цена:": performance_price.text,
-                "Театр:": performance_theatre.text,
-                "Описание:": performance_min_discription.text,
-                "URL спектакля:": performance_urls,
-            }
-        )
-print(performance_data_list)
-with open("all_performance_dict.json", "w", encoding='utf-8') as file:
-    json.dump(performance_data_list, file, indent=4, ensure_ascii=False)
-    # data=json.load(file)
-    # df=pd.DataFrame(data)
-    # df.to_excel("performance_list.xlsx")
-
-with open("all_performance_dict.json", "r", encoding='utf-8') as file:
-    pd.read_json('all_performance_dict.json', lines=True, orient='records')
-    #pd.read_json("all_performance_dict.json", lines=True, orient='records').to_excel("performance_list.xlsx")
-
+main()
 
 # "#Выводим основную информацию с главной страницы"
     #     print(f"Название спектакля: {performance_name.text}")
@@ -90,7 +99,7 @@ with open("all_performance_dict.json", "r", encoding='utf-8') as file:
     #     print(f"Описание: {performance_min_discription.text}")
     #     print("\n")
 
-#"#Создаём словарь из ссылок на спектакли"
+#Создаём словарь из ссылок на спектакли"
     # all_performance_hrefs = soup.find_all(class_="_3NqYW DWsHS _3lmHp wkn_c")
     # all_performance_dict = {}
     # for item in all_performance_hrefs:
--- test.py
+++
@@ -1,3 +0,0 @@
-url = "https://www.afisha.ru/performance/104669/"
-url= url.split("/")[-2]
-print(url)
